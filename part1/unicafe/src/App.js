2529523cfb03a30584ee84d71ce582e38ddb02e9
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2022-12-29 13:05:32 +0200

    finish part1, add part 1.12-1.14

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 const Button = ({ setter, text, counter }) => {
-	console.log(setter)
 	const increaseValue = () => setter(counter + 1)
 	return (
 		<button onClick={increaseValue}>
