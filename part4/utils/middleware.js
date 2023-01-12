2c23dfcda97338fffa14b49a3fdd5e0d8215fd80
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-01-12 19:05:37 +0200

    add part 4.20

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -28,8 +28,18 @@ const errorHandler = (error, request, response, next) => {
 	next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+	const authorization = request.get('authorization')
+	authorization && authorization.toLowerCase().startsWith('bearer ')
+		? request.token = authorization.substring(7)
+		: request.token = null
+
+	next()
+}
+
 module.exports = {
 	requestLogger,
 	unknownEndpoint,
-	errorHandler
+	errorHandler,
+	tokenExtractor
 }
