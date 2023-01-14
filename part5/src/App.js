bd5c1d19eff004afffe98aa029bdaf1dfb9ebe7b
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-01-14 20:11:22 +0200

    add part 5.5-5.6

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,14 +1,15 @@
 import './index.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import NoteForm from './components/NoteForm'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-	const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
 	const [notification, setNotification] = useState(null)
 	const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
@@ -52,30 +53,18 @@ const App = () => {
 		setUser(null)
 	}
 
-	const handleChange = (event) => {
-		console.log(event.target.value)
-		setNewBlog({
-			...newBlog,
-			[event.target.name]: event.target.value
-		})
-		console.log(newBlog)
-	}
-
-	const addBlog = async (event) => {
-		event.preventDefault()
+	const addBlog = async (blogObject) => {
+		noteFormRef.current.toggleVisibility()
 		try {
-			const returnedBlog = await blogService.create({
-				title: newBlog.title,
-				author: newBlog.author,
-				url: newBlog.url,
-			})
+			const returnedBlog = await blogService.create(blogObject)
 			setBlogs(blogs.concat(returnedBlog))
-			setNewBlog({ author: '', title: '', url: '' })
 			showNotification('success', `a new blog "${returnedBlog.title}" by ${returnedBlog.author} added!`)
 		} catch(e) {
 			showNotification('error', e.response.data.error)
 		}
 	}
+
+	const noteFormRef = useRef()
 
   if (user === null) {
     return (
@@ -100,19 +89,9 @@ const App = () => {
 			<p>{user.name} logged in
 				<button onClick={() => handleLogout()}>logout</button>
 			</p> 
-			<h2>create new</h2>
-			<form onSubmit={addBlog}>
-				<div>title:
-					<input name="title" value={newBlog.title} onChange={handleChange}></input>
-				</div>
-				<div>author:
-					<input name="author" value={newBlog.author} onChange={handleChange}></input>
-				</div>
-				<div>url:
-					<input name="url" value={newBlog.url} onChange={handleChange}></input>
-				</div>
-			<button type="submit">create</button>
-			</form>
+			<Togglable buttonLabel="new note" ref={noteFormRef}>
+				<NoteForm createBlog={addBlog} />
+			</Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
 				)}
---
+++ part5/src/components/NoteForm.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const NoteForm = ({ createBlog }) => {
+	const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
+
+	const handleChange = (event) => {
+		console.log(event.target.value)
+		setNewBlog({
+			...newBlog,
+			[event.target.name]: event.target.value
+		})
+	}
+
+	const addBlog = async (event) => {
+		event.preventDefault()
+		createBlog({
+			title: newBlog.title,
+			author: newBlog.author,
+			url: newBlog.url,
+		})
+		setNewBlog({ author: '', title: '', url: '' })
+	}
+	
+	return (
+		<div>
+			<h2>create new</h2>
+
+			<form onSubmit={addBlog}>
+				<div>title:
+					<input name="title" value={newBlog.title} onChange={handleChange}></input>
+				</div>
+				<div>author:
+					<input name="author" value={newBlog.author} onChange={handleChange}></input>
+				</div>
+				<div>url:
+					<input name="url" value={newBlog.url} onChange={handleChange}></input>
+				</div>
+				<button type="submit">create</button>
+			</form>
+			
+		</div>
+	)
+}
+
+export default NoteForm
