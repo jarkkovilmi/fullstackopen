cad86cdffa4e586c3d7680478db0f8fcefbd1cdd
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-01-19 18:22:40 +0200

    add part 6.6

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -42,4 +42,18 @@ const reducer = (state = initialState, action) => {
 	}
 }
 
+export const createVote = (id) => {
+	return { 
+		type: 'VOTE',
+		data: { id }
+	}
+}
+
+export const createAnecdote = (content) => {
+	return { 
+		type: 'NEW_ANECDOTE',
+		data: { content }
+	}
+}
+
 export default reducer
