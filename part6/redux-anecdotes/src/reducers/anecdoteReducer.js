26f90cbd6265cf80fa803ad4acb66801691c1760
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-01-21 12:11:50 +0200

    add part 6.15

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
 	name: 'anecdotes',
@@ -27,4 +28,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const notes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(notes))
+  }
+}
+
 export default anecdoteSlice.reducer
