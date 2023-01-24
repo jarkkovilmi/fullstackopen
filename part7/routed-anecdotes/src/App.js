d205aa9c8001b3c9c2a5f2c694fec971d289916f
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-01-24 10:37:09 +0200

    add part 7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -69,9 +69,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const {reset: contentReset, ...content} = useField('text')
+  const {reset: authorReset, ...author} = useField('text')
+  const {reset: infoReset, ...info} = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -85,9 +85,9 @@ const CreateNew = (props) => {
 
 	const handleReset = (e) => {
 		e.preventDefault()
-		content.reset()
-		author.reset()
-		info.reset()
+		contentReset()
+		authorReset()
+		infoReset()
 	}
 
   return (
