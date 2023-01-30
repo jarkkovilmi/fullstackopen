9aacc1e97c2b3226fc75f0ec6a6968275da30618
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-01-30 19:52:00 +0200

    add part 8.6

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -100,6 +101,15 @@ const typeDefs = gql`
 		allBooks(author: String, genre: String): [Book!]!
 		allAuthors: [Author!]!
   }
+
+	type Mutation {
+    addBook(
+			title: String!
+			published: Int!
+			author: String!
+			genres: [String]
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -118,6 +128,17 @@ const resolvers = {
   },
 	Author: {
 		bookCount: (root) => books.filter(book => book.author === root.name).length
+	},
+	Mutation: {
+		addBook: (root, args) => {
+			const book = { ...args, id: uuid() }
+			books = books.concat(book)
+			if (!authors.find(author => author.name === args.author)) {
+        const author = { name: args.author, id: uuid() }
+				authors = authors.concat(author)
+      }
+			return book
+		}
 	}
 }
 
