497ce44b75cdd822d3a35178315468b0819d34fd
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-02-02 12:08:18 +0200

    add part 8.15

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,7 +1,5 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
 const mongoose = require('mongoose')
-const { v1: uuid } = require('uuid')
-const author = require('./models/author')
 const Author = require('./models/author')
 const Book = require('./models/book')
 require('dotenv').config()
@@ -17,84 +15,6 @@ mongoose.connect(MONGODB_URI)
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
-
-// let authors = [
-//   {
-//     name: 'Robert Martin',
-//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-//     born: 1952,
-//   },
-//   {
-//     name: 'Martin Fowler',
-//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-//     born: 1963
-//   },
-//   {
-//     name: 'Fyodor Dostoevsky',
-//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-//     born: 1821
-//   },
-//   { 
-//     name: 'Joshua Kerievsky', // birthyear not known
-//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-//   },
-//   { 
-//     name: 'Sandi Metz', // birthyear not known
-//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-//   },
-// ]
-
-// let books = [
-//   {
-//     title: 'Clean Code',
-//     published: 2008,
-//     author: 'Robert Martin',
-//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring']
-//   },
-//   {
-//     title: 'Agile software development',
-//     published: 2002,
-//     author: 'Robert Martin',
-//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-//     genres: ['agile', 'patterns', 'design']
-//   },
-//   {
-//     title: 'Refactoring, edition 2',
-//     published: 2018,
-//     author: 'Martin Fowler',
-//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring']
-//   },
-//   {
-//     title: 'Refactoring to patterns',
-//     published: 2008,
-//     author: 'Joshua Kerievsky',
-//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring', 'patterns']
-//   },  
-//   {
-//     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-//     published: 2012,
-//     author: 'Sandi Metz',
-//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring', 'design']
-//   },
-//   {
-//     title: 'Crime and punishment',
-//     published: 1866,
-//     author: 'Fyodor Dostoevsky',
-//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-//     genres: ['classic', 'crime']
-//   },
-//   {
-//     title: 'The Demon ',
-//     published: 1872,
-//     author: 'Fyodor Dostoevsky',
-//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-//     genres: ['classic', 'revolution']
-//   },
-// ]
 
 const typeDefs = gql`
 	type Author {
@@ -157,17 +77,31 @@ const resolvers = {
 			let author = await Author.findOne({ name: args.author })
 			if (!author) {
 				author = new Author({ name: args.author, born: null })
+				try {
 				await author.save()
+				} catch (error) {
+				throw new UserInputError(error.message, { invalidArgs: args })
+				}
 			}
       const book = new Book({ ...args, author })
-      return await book.save()
+			try {
+      	await book.save()
+			} catch (error) {
+				throw new UserInputError(error.message, { invalidArgs: args })
+			}
+			return book
 		},
 		editAuthor: async (root, args) => {
 			const author = await Author.findOne({ name: args.name })
 			if (!author)
 				return null
       author.born = args.setBornTo
-			return await Author.findByIdAndUpdate(author._id, author, { new: true })
+			try {
+				await Author.findByIdAndUpdate(author._id, author, { new: true })
+			} catch (error) {
+				throw new UserInputError(error.message, { invalidArgs: args  })
+			}
+			return author
 		}
 	}
 }
