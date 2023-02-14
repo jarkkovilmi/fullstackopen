d2aa5ff5bc14ed1998fb8180770d5cf2cb985b8f
Jarkko Vilmi <jarkko.vilmi@gmail.com>
2023-02-14 18:19:21 +0200

    add part 9.21

--- part9/patientor-frontend/src/App.tsx
+++ part9/patientor-frontend/src/App.tsx
@@ -1,13 +1,54 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { Button, Divider, Container, Typography } from '@mui/material';
+import { BrowserRouter as Router, Route, Link, Routes, useParams } from "react-router-dom";
+import { Button, Divider, Container, Typography, SvgIcon } from '@mui/material';
+import { Male, Female, Transgender } from '@mui/icons-material';
 
 import { apiBaseUrl } from "./constants";
-import { Patient } from "./types";
+import { Gender, Patient } from "./types";
 
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
+
+const PatientInfoPage = () => {
+	const [patient, setPatient] = useState<Patient | null>(null);
+	const id = useParams().id;
+
+	useEffect(() => {
+		if (!id)
+			return;
+		const fetchPatient = async () => {
+			const patient = await patientService.getPatient(id);
+			setPatient(patient);
+		};
+		fetchPatient();
+	}, [id]);
+
+	if (!patient)
+		return <div>Patient not found</div>;
+
+	const renderGenderIcon = (gender: Gender) => {
+		switch (gender) {
+			case "male": {
+				return <SvgIcon component={Male} />;
+			}
+			case "female": {
+				return <SvgIcon component={Female} />;
+			}
+			case "other": {
+				return <SvgIcon component={Transgender} />;
+			}
+		}
+	};
+
+	return (
+		<div>
+			<h2>{patient.name} {renderGenderIcon(patient.gender)}</h2>
+			<div>ssn: {patient.ssn}</div>
+			<div>occupation: {patient.occupation}</div>
+		</div>
+	);
+};
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
@@ -35,6 +76,7 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route path="/patient/:id" element={<PatientInfoPage />} />
           </Routes>
         </Container>
       </Router>
